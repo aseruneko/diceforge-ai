291337195eb9d455389c945eb607186368d953e3
oz4point5 <trapezohedron0000@gmail.com>
2020-05-14 12:54:00 +0900

    first commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+
+# main.py
+# @author aseruneko
+
+# メインです。このファイルを実行してください。
+
+while True:
+    print("command? > "),
+    command = input()
+    if command == "exit":
+        print("exit")
+        break
+
+def initialize:
+    # ゲームの初期化処理
+
+def commandPerse:
+    # コマンドの解釈
