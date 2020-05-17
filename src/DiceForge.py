970579d5f8dbc47194563a429f28101ad20a58d7
c3441 <masomoi2424@gmail.com>
2020-05-17 20:18:39 +0900

    separete classes 2

---
+++ src/DiceForge.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+class DiceForge:
+
+    # ゲームの実際の流れを実装
+    def play(self):
+        board = Board()
+        player1 = computer = Computer()
+        player2 = user = User()
+
+        round = 0 
+        # 以下ターンが交互に回ってきてラウンドが経過していく処理をDiceForge内に実装したい
+
+    # リソース(カード含める？)を参照して試合結果を表示する　
+    def show_result(self, Resorce):
