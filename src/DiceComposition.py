977f1c7153001522a1c5615b1917db5b4535b8c4
yochi <yochi.orz@gmail.com>
2020-05-18 22:03:52 +0900

    we can play game. mod DiceComposition, DiceForge, Player

--- src/DiceComposition.py
+++ src/DiceComposition.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-import random
+"""
+define dice
+"""
+
 
 __author__ = "willofhare aka yochi"
 __status__ = "production"
 __date__ = "17 May 2020"
 
+import random
 
-class Dices:
-    def __init__(self):#ダイスを二つ持っている
-        self.dice1 = Dice()
-        self.dice2 = Dice()
-    
-    def role(self):#ダイスを振って出目を変更する
-        self.dice1.roll()
-        self.dice2.roll()
-
-    def effect(self, player):#今出ている出目の効果を解決する
-        self.dice1.effect()
-        self.dice2.effect()
-
-
-    
 class Dice:
     def __init__(self):#ダイスの目を初期化。仮実装
         self.faces = [
@@ -35,8 +24,8 @@ class Dice:
         ]
         self.roll()
 
-    def role(self):#ランダムに振って出目のfaceオブジェクトを返す
-        self.top = self.faces.choice()
+    def roll(self):#ランダムに振って出目のfaceオブジェクトを返す
+        self.top = random.choice(self.faces)
     
     def choice(self, num):#num番目の出目を返す
         return self.faces[num]
@@ -51,39 +40,39 @@ class Face:
 
     def __single_effect(self, tag, val, player):#実際に起きるエフェクト
         if tag == "gold":
-            player.resource.add_gold(self.val)
+            player.resource.add_gold(val)
         if tag == "vp":
-            player.resource.add_victory_point(self.val)
+            player.resource.add_victory_point(val)
         if tag == "sun":
-            player.resource.add_sun(self.val)
+            player.resource.add_sun(val)
         if tag == "moon":
-            player.resource.add_moon(self.val)
+            player.resource.add_moon(val)
 
     def effect(self, player):
         if self.tag in ["gold", "vp", "sun", "moon"]:
-            __single_effect(self.tag, self.val, player)
+            self.__single_effect(self.tag, self.val, player)
 
         if self.tag == "+":
             for item in self.val:
-                __single_effect(item[0], item[1], player)
+                self.__single_effect(item[0], item[1], player)
 
         if self.tag == "?":
-            item = self.val.choice()#仮実装
-            __single_effect(item[0], item[1], player)
+            item = random.choice(self.val)#仮実装
+            self.__single_effect(item[0], item[1], player)
 
         if self.tag == "*3":
             of = __get_other_face()
             if of.tag in ["gold", "vp", "sun", "moon"]:
-                __single_effect(of.tag, of.val * 3, player)
+                self.__single_effect(of.tag, of.val * 3, player)
             if of.tag == "+":
                 for item in of.val:
-                    __single_effect(item[0], item[1] * 3, player)
+                    self.__single_effect(item[0], item[1] * 3, player)
             if of.tag == "?":
-                item = self.val.choice()#仮実装
-                __single_effect(item[0], item[1] * 3, player)
+                item = random.choice(self.val)#仮実装
+                self.__single_effect(item[0], item[1] * 3, player)
 
         if self.tag == "mirror":
-            of = __get_other_face()
+            of = self.__get_other_face()
             of.effect(player)
 
     def __get_other_face(self):
