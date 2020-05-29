e1697e7956d5584b90c1b7baf3ea316115a5bdd6
aseruneko <aseruneko@gmail.com>
2020-05-29 14:34:39 +0900

    add description in Human.py

--- src/main/Human.py
+++ src/main/Human.py
@@ -2,15 +2,28 @@
 # -*- coding:utf-8 -*-
 
 """
-Class of Human
+プレイヤーのうち、人間が操作するクラス
+"""
+
+"""
+- Playerクラスを継承している
+- 全体的に未実装
 """
 
 __author__ = "aseruneko"
-__date__ = "28 May 2020"
+__date__ = "29 May 2020"
 
 from main.Player import Player
 
 class Human(Player):
+
+    """
+    [インスタンスメソッド]
+        
+        action(board, resorce, cards):
+            作った気はするが実際どういう実装にするべきか忘れ去られた可愛そうなメソッド。
+            今の所、どこにも使用されていない。
+    """
     
     # プレイヤーの手番の処理を行う関数
     def action(self, board, resorce, cards):
