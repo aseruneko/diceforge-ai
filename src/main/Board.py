3c74daccaa80d19cac5c2f0c6aed6d7f8512fc64
aseruneko <aseruneko@gmail.com>
2020-05-30 19:41:17 +0900

    remove test code in Board.py

--- src/main/Board.py
+++ src/main/Board.py
@@ -66,12 +66,3 @@ class Board(IOInterface):
     def write(self, string):
         print("")
         print(string)
-        
-
- 
-#test
-if __name__ == '__main__':
-    board = Board(Card_Types)
-    print(board.remain_faces) # ex.[0, 0, 2, 2, 2, 2, 1, 0, 0, 1, 2, 2, 2, 2, 1, 1, 0, 0]
-    print(board.remain_cards) # [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-    print(board.used_CardType ) # [['HAMMER', 0, 1, 0], ['CHEST', 0, 1, 2], ['HIND', 0, 2, 2], ['SATYRS', 0, 3, 6], ['FERRYMAN', 0, 4, 12], ['HELMET', 0, 5, 4], ['CANCER', 0, 6, 8], ['ELDER', 1, 0, 0], ['SPIRIT', 1, 0, 2], ['OWL', 2, 0, 4], ['MINO', 3, 0, 8], ['GORGON', 4, 0, 14], ['MIRROR', 5, 0, 10], ['SPHINX', 6, 0, 10], ['HYDRA', 5, 5, 26]]
